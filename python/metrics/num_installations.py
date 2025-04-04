b2b2f06564b0ff645b13c396d561a0569a196c5e
Philip Durbin <philip_durbin@harvard.edu>
2025-04-04 15:55:14 -0400

    if no -d arg, use current month

--- python/metrics/num_installations.py
+++ python/metrics/num_installations.py
@@ -24,23 +24,16 @@ def main():
         action="store_true",
     )
     args = parser.parse_args()
-    base_url = "https://hub.dataverse.org/api/installation/metrics/monthly"
-    date = ""
+
+    desired_month = None
+
     if args.date:
         desired_month = parse_date(args.date)
-        # Figure out next month, needed to pass to API
-        year = desired_month.year
-        month = desired_month.month + 1
-        if month > 12:  # Handle year rollover
-            month = 1
-            year += 1
-        next_month = datetime(year, month, 1)
-        from_date = desired_month.strftime("%Y-%m")
-        to_date = next_month.strftime("%Y-%m")
-        date = f"?fromDate={from_date}&toDate={to_date}"
+    else:
+        desired_month = datetime.now()
 
-    # For March 2025 we use https://hub.dataverse.org/api/installation/metrics/monthly?fromDate=2025-03&toDate=2025-04
-    url = base_url + date
+    url = construct_url(desired_month)
+
     if args.verbose:
         print(f"Fetching data from {url}")
 
@@ -59,6 +52,22 @@ def parse_date(date_str):
     except ValueError as e:
         print(f"Invalid date format: {date_str}. Expected format is YYYY-MM.")
         sys.exit(1)
+
+
+def construct_url(desired_month):
+    # Figure out next month, needed to pass to API
+    year = desired_month.year
+    month = desired_month.month + 1
+    if month > 12:  # Handle year rollover
+        month = 1
+        year += 1
+    next_month = datetime(year, month, 1)
+    from_date = desired_month.strftime("%Y-%m")
+    to_date = next_month.strftime("%Y-%m")
+    # For March 2025 we use https://hub.dataverse.org/api/installation/metrics/monthly?fromDate=2025-03&toDate=2025-04
+    # Don't use just https://hub.dataverse.org/api/installation/metrics/monthly to get the current month
+    # because it will return installations that were ever able to be polled.
+    return f"https://hub.dataverse.org/api/installation/metrics/monthly?fromDate={from_date}&toDate={to_date}"
 
 
 def fetch_metrics(url):
